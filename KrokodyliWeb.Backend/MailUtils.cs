37bfa48ad876243a37f5f1a2e45cfc958e7e81a5
MarkusSecundus <kubahronik.g@gmail.com>
2022-07-30 22:42:25 +0200

    implemented basic mailreading functionality (should work with gmail)

---
+++ KrokodyliWeb.Backend/MailUtils.cs
@@ -0,0 +1,20 @@
+﻿using MailKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrokodyliWeb.Backend
+{
+    internal static class MailUtils
+    {
+        public static void MarkRead(this IMailFolder self, UniqueId uid, bool read=true)
+        {
+            if (read)
+                self.AddFlags(uid, MessageFlags.Seen, true);
+            else
+                self.RemoveFlags(uid, MessageFlags.Seen, true);
+        }
+    }
+}
--- KrokodyliWeb.Backend/Program.cs
+++ KrokodyliWeb.Backend/Program.cs
@@ -1,10 +1,53 @@
-﻿namespace KrokodyliWeb.Backend
+﻿using CommandLine;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MailKit.Security;
+
+namespace KrokodyliWeb.Backend
 {
-    internal class Program
+    public class BackendEntryPoint
     {
-        static void Main(string[] args)
+        public class CmdArgs
         {
-            Console.WriteLine("Hello, World!");
+            [Option('e', "eventlist", Required = true, HelpText = "Path to the json file listing the events")]
+            public string EventListPath { get; init; } = null!;
+            [Option('h', "imaphostname", Required = false, HelpText = "Hostname of the imap server")]
+            public string ImapHostName { get; init; } = "imap.gmail.com";
+            [Option('p', "imapport", Required = false, HelpText = "Port of the imap server")]
+            public int ImapPort { get; init; } = 993;
+
+
+            [Option('u', "mailusername", Required = true, HelpText = "Username for the mail client")]
+            public string MailUserName { get; init; } = null!;
+            [Option('w', "mailpassword", Required = true, HelpText = "Password for the mail client")]
+            public string MailPassword { get; init; } = null!;
+        }
+
+        public static void Main(string[] args)
+        {
+            var parsed = Parser.Default.ParseArguments<CmdArgs>(args);
+            if (parsed.Value == null) return;
+            Main(parsed.Value);
+        }
+
+#pragma warning disable CS0028
+        public static void Main(CmdArgs args)
+#pragma warning restore CS0028
+        {
+            Console.WriteLine($"user: '{args.MailUserName}'\npasswd: '{args.MailPassword}'");
+            using var client = new ImapClient();
+            client.Connect(args.ImapHostName, args.ImapPort, SecureSocketOptions.SslOnConnect);
+            client.Authenticate(args.MailUserName, args.MailPassword);
+            client.Inbox.Open(FolderAccess.ReadWrite);
+            
+
+            foreach(var uid in client.Inbox.Search(SearchQuery.NotSeen))
+            {
+                var message = client.Inbox.GetMessage(uid);
+                Console.WriteLine($"{uid}--'{message.Subject}':\n{message.TextBody.Substring(0, 10)}...\n\n");
+                client.Inbox.MarkRead(uid);
+            }
         }
     }
 }
