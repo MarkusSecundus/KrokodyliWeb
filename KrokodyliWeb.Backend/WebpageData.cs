6327b8cb21d9a246be9472f43a68d2b349b87a28
MarkusSecundus <kubahronik.g@gmail.com>
2022-07-30 23:34:20 +0200

    added class to represent webpage data

---
+++ KrokodyliWeb.Backend/WebpageData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrokodyliWeb.Backend
+{
+    internal class WebpageData
+    {
+        public List<EventInfo>? Events { get; set; }
+
+        public class EventInfo
+        {
+            public string? RawText { get; set; }
+
+            public List<string>? AttachedImagesUrl { get; set; }
+        }
+    }
+}
