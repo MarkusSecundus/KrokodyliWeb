b4849f8c729ce7e6f5e587394fd3f558adfb6677
MarkusSecundus <kubahronik.g@gmail.com>
2022-07-30 18:22:35 +0200

    Přidejte soubory projektu.

---
+++ KrokodyliWeb.Backend/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KrokodyliWeb.Backend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ KrokodyliWeb.Frontend/Client/Program.cs
@@ -0,0 +1,20 @@
+using KrokodyliWeb.Frontend;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace KrokodyliWeb.Frontend
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
