aebf7656d5460633728a14c9c2ef8787834b9e09
MarkusSecundus <kubahronik.g@gmail.com>
2022-08-01 23:34:09 +0200

    some progress

--- KrokodyliWeb.Frontend/Client/Program.cs
+++ KrokodyliWeb.Frontend/Client/Program.cs
@@ -20,7 +20,7 @@ namespace KrokodyliWeb.Frontend
             builder.Services.AddSingleton<WebpageConfig>(sp => new());
             builder.Services.AddSingleton<WebpageData>(sp =>
             {
-                return new();
+                return new() { Contacts = new() { new WebpageData.ContactsInfo { Email = "krokodyli@seznam.cz", PersonName = "Martina Barvíøová", PhoneNumber = "123456789" } } };
                 /*var cfg = sp.GetRequiredService<WebpageConfig>();
                 var httpClient = sp.GetRequiredService<HttpClient>();
                 return httpClient.GetJsonAsync<WebpageData>(cfg.DataFileURI).Result;*/
