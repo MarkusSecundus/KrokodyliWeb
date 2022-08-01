710a04effb17f39e9f2a9e8de97213477b46194c
MarkusSecundus <kubahronik.g@gmail.com>
2022-08-01 14:21:28 +0200

    refactor

--- KrokodyliWeb.Backend/BackendEntryPoint.cs
+++ KrokodyliWeb.Backend/BackendEntryPoint.cs
@@ -1,9 +1,11 @@
 ﻿using CommandLine;
+using KrokodyliWeb.Data;
 using KrokodyliWeb.Utils;
 using MailKit;
 using MailKit.Net.Imap;
 using MailKit.Search;
 using MailKit.Security;
+using System.Text.Json;
 
 namespace KrokodyliWeb.Backend
 {
@@ -29,32 +31,60 @@ namespace KrokodyliWeb.Backend
         {
             var parsed = Parser.Default.ParseArguments<CmdArgs>(args);
             if (parsed.Value == null) return;
-            Main(parsed.Value);
+            new BackendEntryPoint(parsed.Value).Run();
         }
 
-#pragma warning disable CS0028
-        public static void Main(CmdArgs args)
-#pragma warning restore CS0028
+
+
+        private readonly CmdArgs args;
+        private readonly WebpageData data;
+        public BackendEntryPoint(CmdArgs args) {
+            this.args = args;
+            this.data = GetWebpageData(args);
+        }
+
+
+        public void Run()
+        {
+
+            SaveWebpageData();
+        }
+
+
+
+        private static WebpageData GetWebpageData(CmdArgs args)
+        {
+            try
+            {
+                using var file = File.OpenRead(args.DataFilePath);
+                return JsonSerializer.Deserialize<WebpageData>(file) ?? new();
+            }catch(Exception e) when (e is IOException || e is JsonException)
+            {
+                return new();
+            }
+        }
+
+        private void SaveWebpageData()
+        {
+            using var file = File.OpenWrite(args.DataFilePath);
+            JsonSerializer.Serialize(file, data);
+        }
+
+        private static void ProcessMails(CmdArgs args)
         {
             Console.WriteLine($"user: '{args.MailUserName}'\npasswd: '{args.MailPassword}'");
             using var client = new ImapClient();
             client.Connect(args.ImapHostName, args.ImapPort, SecureSocketOptions.SslOnConnect);
             client.Authenticate(args.MailUserName, args.MailPassword);
             client.Inbox.Open(FolderAccess.ReadWrite);
-            
 
-            foreach(var uid in client.Inbox.Search(SearchQuery.NotSeen))
+
+            foreach (var uid in client.Inbox.Search(SearchQuery.NotSeen))
             {
                 var message = client.Inbox.GetMessage(uid);
                 Console.WriteLine($"{uid}--'{message.Subject}':\n{message.TextBody.Substring(0, 10)}...\n\n");
                 client.Inbox.MarkRead(uid);
             }
-        }
-
-
-        void s()
-        {
-            //Google.Apis.Drive.v3.Data.
         }
     }
 }
