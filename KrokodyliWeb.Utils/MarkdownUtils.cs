a7be5074edaa0a56c8d746b0c968514fc9543583
MarkusSecundus <kubahronik.g@gmail.com>
2022-08-21 23:39:10 +0200

    added some utilities for processing markdown

---
+++ KrokodyliWeb.Utils/MarkdownUtils.cs
@@ -0,0 +1,21 @@
+﻿using Markdig;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrokodyliWeb.Utils
+{
+    public static class MarkdownUtils
+    {
+        private static class MarkdownPipeline_LazyInit
+        {
+            public static readonly MarkdownPipeline Value = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+        }
+        public static string MarkdownToHtml(string markdown, bool useBootstrap=false)
+        {
+            return Markdown.ToHtml(markdown, MarkdownPipeline_LazyInit.Value);
+        }
+    }
+}
