86b7c99dc77ab35ebff096c3e7c876026099623e
MarkusSecundus <kubahronik.g@gmail.com>
2022-08-23 00:33:23 +0200

    refactor - added HtmlAgilityPack nuget

--- KrokodyliWeb.Utils/MarkdownUtils.cs
+++ KrokodyliWeb.Utils/MarkdownUtils.cs
@@ -1,4 +1,5 @@
-﻿using Markdig;
+﻿using HtmlAgilityPack;
+using Markdig;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,35 @@ namespace KrokodyliWeb.Utils
             return ret;
         }
 
+
+
         private static string BeautifyHtml(string html)
         {
-            return html.Replace("<ol>", @"<ol class=""list-group list-group-numbered text-white"">");
+            var doc = CreateHtmlDocument(html);
+
+            HtmlNodeCollection ols = doc.DocumentNode.SelectNodes("descendant::ol");
+            if (ols == null) return html;
+
+            foreach (var ol in ols)
+                ol.AddClass("list-group list-group-numbered text-white");
+
+            return doc.SaveString();
+        }
+
+
+
+
+        private static HtmlDocument CreateHtmlDocument(string html)
+        {
+            HtmlDocument ret = new();
+            ret.LoadHtml(html);
+            return ret;
+        }
+        private static string SaveString(this HtmlDocument self)
+        {
+            using var ret = new StringWriter();
+            self.Save(ret);
+            return ret.ToString();
         }
     }
 }
