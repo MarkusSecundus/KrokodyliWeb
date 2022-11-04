a981ae9abb30d070ca359dc1169c5f5376eeac0f
MarkusSecundus <kubahronik.g@gmail.com>
2022-11-04 15:12:33 +0100

    fix base address problems

---
+++ KrokodyliWeb.Frontend/wwwroot/js/adjust_base_tag.js
@@ -0,0 +1,7 @@
+﻿let repoPart = '/' + (await(await fetch('repo_info.json')).json()).GithubInfo.RepoName;
+let baseHref = window.location.pathname.startsWith(repoName)
+    ? repoPart
+    : '/'
+    ;
+
+$('head  base').attr('href', baseHref);
