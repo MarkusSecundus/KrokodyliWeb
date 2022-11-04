db418eeb4dd1d6fd887254550e2f2e7099ab792f
MarkusSecundus <62842675+MarkusSecundus@users.noreply.github.com>
2022-11-04 15:21:46 +0000

    Deploying to gh-pages from  @ 1366d16a53ce2ef6c9ec79c40048024badd7e3be 🚀

--- js/adjust_base_tag.js
+++
@@ -1,7 +0,0 @@
-﻿let repoPart = '/' + (await(await fetch('repo_info.json')).json()).GithubInfo.RepoName;
-let baseHref = window.location.pathname.startsWith(repoName)
-    ? repoPart
-    : '/'
-    ;
-
-$('head  base').attr('href', baseHref);
