aae1bf3f16566b93a2c197ebce9422e752b77f16
MarkusSecundus <62842675+MarkusSecundus@users.noreply.github.com>
2023-01-16 21:46:38 +0000

    Deploying to pages-raw from  @ a6ea7f8bfe4fec053c48b6c792bd7b16a7fdfd91 🚀

---
+++ js/markdown_fragment.js
@@ -0,0 +1,25 @@
+﻿
+window.markdown_fragment = {
+    beautify_ols: function (root) {
+        root.find('ol').addClass("list-group list-group-numbered text-white")
+    },
+
+    make_windows_clickable: function (root, clickator) {
+        root.find('img').each(function () {
+            const self = $(this);
+            clickator.invokeMethodAsync('MakeCallback', self.attr('src'), self.attr('alt'))
+                .then(function (callback) {
+                    self.click(function () {
+                        callback.invokeMethodAsync('Click');
+                    });
+                });
+        });
+    },
+
+
+    beautify: function (rootRef, clickabilityObj) {
+        const root = $(rootRef);
+        this.beautify_ols(root);
+        clickabilityObj && this.make_windows_clickable(root, clickabilityObj);
+    }
+};
